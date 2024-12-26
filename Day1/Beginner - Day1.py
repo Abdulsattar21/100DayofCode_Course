9f7f4e64d1434febc6e42d9b247b43bcb7238e3f
Abdul <abdalsattarpro@gmail.com>
2024-12-26 19:11:18 +0300

    day1 added

---
+++ Day1/Beginner - Day1.py
@@ -0,0 +1,31 @@
+"""This is the day 1 over here... """
+"""
+----------------------------------------------------------------------
+ Select the code and then press ctrl + / to uncomment the code
+"""
+# import sys
+# print(" Hello Wor ld!")
+# print("Welcome to my \n World!")
+# # this is a kind of what called nested code
+# print("Hello " + input("What is your name?"))
+# # There is a program (compiler) called Thonny and it's for compiling step by step
+# # ctrl + / to convert a comment
+# print("The end..")
+# name = input("what is your name?")
+# print(sys.getsizeof(name)) # size of the string in bytes
+# print(len(name))
+# # or you can write like this:
+# print(len(input("what is your name? ")))
+# # switching numbers
+# num1 = input("enter a num 1: ")
+# num2 = input("enter a num 2: ")
+# print(num1 + " " + num2)
+# num0 = num1
+# num1 = num2
+# num2 = num0
+# print(num1 + " " + num2)
+print("Hello in my program")
+city_name= input("from where are you? \n")
+pet_name =  input("what is your pet's name? \n")
+print("your brand name is: " + city_name +" "+ pet_name + "\nThanks for using my program")
+
