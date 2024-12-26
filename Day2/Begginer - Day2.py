a57138fd4c50cb14b89d50d188bac1a3d18b176c
Abdul <abdalsattarpro@gmail.com>
2024-12-26 19:14:03 +0300

    Day2

---
+++ Day2/Begginer - Day2.py
@@ -0,0 +1,19 @@
+"""     Day2 : Data Types      """
+import random
+# string:
+print("Hello"[0])
+# taking the first char from the string (start counting from 0)
+int = 3
+print(type(int))
+# this will just help you to find the data type of a vari
+new_int = str(int) # convert int to string
+print(new_int)
+
+# num1 = input("enter 1st num: ")
+# n1 = int(num1)
+# num2 = input("enter 2nd num: ")
+# n2 = int(num2)
+# n3 = n1 + n2
+# print(n3)
+
+print(" _ ")
